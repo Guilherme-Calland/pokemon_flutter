29fe6e33e746774704aab8ffbc926e09f086eb28
guilhermow <gui.crb0@gmail.com>
2020-08-19 18:01:00 -0300

    opening pokedex

---
+++ pokemon_flutter_app/lib/components/components.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+AnimationController rotateAnimationController, moveAnimationController;
+Animation rotateAnimation, moveAnimation;
+
--- pokemon_flutter_app/lib/main.dart
+++ pokemon_flutter_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'components/components.dart';
+
 main(){
   runApp(MaterialApp(
     home: Pokedex(),
@@ -12,7 +14,36 @@ class Pokedex extends StatefulWidget {
   _PokedexState createState() => _PokedexState();
 }
 
-class _PokedexState extends State<Pokedex> {
+class _PokedexState extends State<Pokedex> with TickerProviderStateMixin{
+
+  @override
+  void initState() {
+    super.initState();
+    setRotationAnimation();
+    moveAnimationController = AnimationController(
+        duration: Duration(milliseconds: 1000),
+        vsync: this,
+        value: 0.0
+    );
+    moveAnimation = Tween<double>(begin:0, end: 300).animate(moveAnimationController)..addListener(() {
+      setState(() {});
+    });
+  }
+
+  void setRotationAnimation() {
+    rotateAnimationController = AnimationController(
+        duration: Duration(milliseconds: 500),
+        vsync: this,
+        value: 0.0
+    );
+    rotateAnimation = Tween<double>(begin:0.8, end: 0.0).animate(rotateAnimationController)..addListener(() {
+      if(rotateAnimation.isCompleted){
+        moveAnimationController.forward();
+      }
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -21,46 +52,71 @@ class _PokedexState extends State<Pokedex> {
     return Scaffold(
       body: Stack(
         children: [
+          Scaffold(
+            backgroundColor: Colors.grey,
+            body: Center(
+              child: Text('POKEDEX', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),)
+            )
+          ),
           Column(
             children: [
-              Container(
-                height: screenHeight/2,
-                width: screenWidth,
-                color: Colors.red,
+              Transform.translate(
+                offset: Offset(0,- moveAnimation.value),
+                child: Container(
+                  height: screenHeight/2,
+                  width: screenWidth,
+                  color: Colors.red,
+                ),
               ),
-              Container(
-                height: screenHeight/2,
-                width: screenWidth,
-                color: Colors.white,
+              Transform.translate(
+                offset: Offset(0, moveAnimation.value),
+                child: Container(
+                  height: screenHeight/2,
+                  width: screenWidth,
+                  color: Colors.white,
+                ),
               )
             ],
           ),
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 50,
-                  width: screenWidth,
-                  color: Colors.black,
-                ),
-              ),
-              Center(
-                child: Transform.rotate(
-                  angle: 0.8,
-                  child: Container(
-                    height: 120,
-                    width: 120,
-//                    color: Colors.white,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(
-                        width: 30
-                      )
+          GestureDetector(
+            onTap: (){
+              rotateAnimationController.forward();
+            },
+            child: Transform.translate(
+              offset: Offset(0, moveAnimation.value),
+              child: Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 50,
+                      width: screenWidth,
+                      color: Colors.black,
                     ),
                   ),
-                )
-              )
-            ],
+                  GestureDetector(
+                    onTap: (){
+                      rotateAnimationController.forward();
+                    },
+                    child: Center(
+                      child: Transform.rotate(
+                        angle: rotateAnimation.value,
+                        child: Container(
+                          height: 120,
+                          width: 120,
+//                    color: Colors.white,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            border: Border.all(
+                              width: 30,
+                            )
+                          ),
+                        ),
+                      )
+                    ),
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
