08ac6f4b558db9527efee93269ff85d4f2474ca3
guilhermow <gui.crb0@gmail.com>
2020-08-19 20:12:26 -0300

    menu.screen

--- pokemon_flutter_app/lib/screens/menu.screen.dart
+++ pokemon_flutter_app/lib/screens/menu.screen.dart
@@ -8,9 +8,29 @@ class MenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey,
-        body: Center(
-            child: Text('Pokedex', style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),)
+      backgroundColor: Colors.black12,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                      Icons.menu,
+                      size: 30,
+                      color: Colors.black
+                  ),
+                  SizedBox(width: 5,),
+                  Text('Pokemon App', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+
+                ],
+              ),
+              FlatButton(
+
+              ),
+            ],
+          ),
         )
     );
   }
