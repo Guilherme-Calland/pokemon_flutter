52b954997d3713592d6f92edb9404813ef1b4f79
guilhermow <gui.crb0@gmail.com>
2020-08-19 16:22:51 -0300

    display screen

--- pokemon_flutter_app/lib/main.dart
+++ pokemon_flutter_app/lib/main.dart
@@ -7,12 +7,62 @@ main(){
   ));
 }
 
-class Pokedex extends StatelessWidget {
+class Pokedex extends StatefulWidget {
+  @override
+  _PokedexState createState() => _PokedexState();
+}
+
+class _PokedexState extends State<Pokedex> {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Center(
-        child: Text('Pokedex'),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                height: screenHeight/2,
+                width: screenWidth,
+                color: Colors.red,
+              ),
+              Container(
+                height: screenHeight/2,
+                width: screenWidth,
+                color: Colors.white,
+              )
+            ],
+          ),
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 50,
+                  width: screenWidth,
+                  color: Colors.black,
+                ),
+              ),
+              Center(
+                child: Transform.rotate(
+                  angle: 0.8,
+                  child: Container(
+                    height: 120,
+                    width: 120,
+//                    color: Colors.white,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        width: 30
+                      )
+                    ),
+                  ),
+                )
+              )
+            ],
+          )
+        ],
       ),
     );
   }
