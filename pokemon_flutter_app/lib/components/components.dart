4893fd8feebd8c388eef2831f4faeccef8716392
guilhermow <gui.crb0@gmail.com>
2020-08-20 12:37:42 -0300

    menu design complete

--- pokemon_flutter_app/lib/components/components.dart
+++ pokemon_flutter_app/lib/components/components.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+TextStyle kDefaultTextStyle = TextStyle(fontSize: 20, color: Color(0x8021ff00), fontFamily: 'PressStart2P');
+
 AnimationController rotateSquareAnimationController, moveAnimationController, moveSquareAnimationController;
 Animation rotateSquareAnimation, moveAnimation, moveSquareAnimation;
 
--- pokemon_flutter_app/lib/screens/menu.screen.dart
+++ pokemon_flutter_app/lib/screens/menu.screen.dart
@@ -1,37 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_flutter_app/components/components.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({
     Key key,
   }) : super(key: key);
 
+//TODO: remove unused assets in the assets folder
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff282828),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Icon(
-                      Icons.menu,
-                      size: 30,
-                      color: Colors.black
-                  ),
-                  SizedBox(width: 5,),
-                  Text('Pokemon App', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //TODO: add the player's name here
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, bottom: 16),
+              child: Text('Welcome', style: kDefaultTextStyle),
+            ),
+            MenuButton(
+              text: 'Your Profile',
+            ),
+            MenuButton(text: 'Search Pokemon'),
+            MenuButton(text: 'Forum'),
+            SizedBox(
+              height: 16,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-                ],
-              ),
-              FlatButton(
+class MenuButton extends StatelessWidget {
+  String text = '';
+  Function onTap;
 
-              ),
-            ],
+  MenuButton({this.text, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 80,
+        margin: EdgeInsets.only(top: 16, bottom: 4),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/buttonBackground.png'),
+        )),
+        child: Center(
+          child: Text(
+            text,
+            style: kDefaultTextStyle,
           ),
-        )
+        ),
+      ),
     );
   }
 }
